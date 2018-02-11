734ec5d741400549f9939b7918e4e44dd0eb19c1
Tiia Björk <tiia.bjork@gmail.com>
2018-02-11 20:05:56 +0200

    Kaikki tehtävät paitsi 3.17 tehty

--- models/person.js
+++ models/person.js
@@ -1,4 +1,8 @@
 const mongoose = require('mongoose')
+
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
 var Schema = mongoose.Schema
 
 const url = process.env.MONGODB_URI
